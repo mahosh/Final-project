75ad2d0784e8aa0420c8402c343a75a1b0abf738
Maor Berkover <mahosh1983@gmail.com>
2020-01-30 13:22:09 +0200

    Week compoment start

---
+++ src/Week.js
@@ -0,0 +1,45 @@
+import Table from 'react-bootstrap/Table';
+import React, { Component } from 'react';
+
+
+
+export default class Week extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentDay: "",
+            firstDayOfTheWeek: "",
+        }
+    }
+    render() {
+        return(
+            <Table responsive>
+                <thead>
+                    <tr>
+                        <th>יום א</th>
+                        <th>יום ב</th>
+                        <th>יום ג</th>
+                        <th>יום ד</th>
+                        <th>יום ה</th>
+                        <th>יום ו</th>
+                        <th>יום ש</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                        <td>Table cell</td>
+                    </tr>
+                </tbody>
+            </Table>
+        )
+    }
+
+}
+
