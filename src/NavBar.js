0cb8f856b02daeb55ff882ee70e2210c9ac5c8fb
Maor Berkover <mahosh1983@gmail.com>
2020-02-09 20:27:13 +0200

    css Nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
+import './NavBar.css';
 
 export default class NavBar extends Component {
     constructor(props) {
@@ -10,17 +11,19 @@ export default class NavBar extends Component {
     render() {
         return(
             <Nav variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">יומני אכילה</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                    לעתיד לבוא
-                </Nav.Link>
-                </Nav.Item>
+                <div className="navItemWrapper">
+                    <Nav.Item>
+                        <Nav.Link >יומני אכילה</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link >Option 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                        לעתיד לבוא
+                    </Nav.Link>
+                    </Nav.Item>
+                </div>
             </Nav>
         )
     }
