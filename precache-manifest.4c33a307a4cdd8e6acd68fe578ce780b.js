8686de34d22ab73671c2533b6c731ccbf8d8f36d
Maor Berkover <mahosh1983@gmail.com>
2020-02-10 09:20:32 +0200

    Updates

---
+++ precache-manifest.4c33a307a4cdd8e6acd68fe578ce780b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6163ea6ecaf1fea1f94d49c51ce3590b",
+    "url": "/Final-project/index.html"
+  },
+  {
+    "revision": "aa3657bcd337f8111994",
+    "url": "/Final-project/static/css/2.3a180413.chunk.css"
+  },
+  {
+    "revision": "7180fb775e7b01e66a7b",
+    "url": "/Final-project/static/css/main.1f5d033b.chunk.css"
+  },
+  {
+    "revision": "aa3657bcd337f8111994",
+    "url": "/Final-project/static/js/2.f496c2e7.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Final-project/static/js/2.f496c2e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7180fb775e7b01e66a7b",
+    "url": "/Final-project/static/js/main.ae83069a.chunk.js"
+  },
+  {
+    "revision": "93a63c848827b0b2e34f",
+    "url": "/Final-project/static/js/runtime-main.d5606da7.js"
+  },
+  {
+    "revision": "9f20282fe9391857e969872f1c44bbe5",
+    "url": "/Final-project/static/media/backgroundImage.9f20282f.jpg"
+  }
+]);
